b9eb2712462455fdcf9a3e4238f5a2169db30a54
morenowtf <tvoibros@gmail.com>
2025-09-22 19:29:25 +0300

    Third exercise finished

--- src/lab01/03_discount_vat.py
+++ src/lab01/03_discount_vat.py
@@ -6,7 +6,7 @@ base = price * (1 - discount / 100)
 vat_amount = base * (vat/100)
 total = base + vat_amount
 
-print(f"База после скидки: {base} \nНДС: {vat_amount} \nИтого к оплате: {total}")
+print(f"База после скидки: {base:.2f} ₽ \nНДС: {vat_amount:.2f} ₽ \nИтого к оплате: {total:.2f} ₽")
 #База после скидки: 900.00 ₽
 #НДС:               180.00 ₽
 #Итого к оплате:    1080.00 ₽
