e0c3b1261b9109f99be5d848798797244898c066
morenowtf <tvoibros@gmail.com>
2025-09-24 16:02:47 +0300

    04 fix

--- src/lab01/04_minutes_to_hhmm.py
+++ src/lab01/04_minutes_to_hhmm.py
@@ -1,6 +1,11 @@
 minutes = int(input("Введите минуты: "))
 
-hours = minutes // 60
-remaining_minutes = minutes % 60
+days = minutes // (24 * 60)
+remaining_minutes_after_days = minutes % (24 * 60)
+hours = remaining_minutes_after_days // 60
+remaining_minutes = remaining_minutes_after_days % 60
 
-print(f"Минуты: {minutes} \n{hours}:{remaining_minutes:02d}")
+if days > 0:
+    print(f"Минуты: {minutes} \n{days} сутки, {hours}:{remaining_minutes:02d}")
+else:
+    print(f"Минуты: {minutes} \n{hours}:{remaining_minutes:02d}")
