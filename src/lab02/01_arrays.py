a89ce3cebd1fe5fb6b62852857976843f54733cb
morenowtf <tvoibros@gmail.com>
2025-10-08 12:19:08 +0300

    fixes

--- src/lab02/01_arrays.py
+++ src/lab02/01_arrays.py
@@ -1,10 +1,10 @@
 #ПЕРВАЯ ФУНЦИЯ
-def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
-    min_number = min(nums)
-    max_number = max(nums)
-
+"""def min_max(nums: list[float | int]) -> tuple[float | int, float | int]:
     if not nums:
         raise ValueError("Вы не ввели число!")
+
+    min_number = min(nums)
+    max_number = max(nums)
 
     return (min_number, max_number)
 
@@ -13,7 +13,7 @@ print(min_max([3, -1, 5, 5, 0]))
 print(min_max([42]))
 print(min_max([-5, -2, -9]))
 print(min_max([1.5, 2, 2.0, -3.1]))
-print(min_max([]))
+print(min_max([]))"""
 
 
 #ВТОРАЯ ФУНКЦИЯ
@@ -22,8 +22,8 @@ print(min_max([]))
     unique_nums.sort()
     return unique_nums
 
-    #if not nums:
-        #raise ValueError("Вы не ввели число!")
+    if not nums:
+        raise ValueError("Вы не ввели число!")
 
 #ТЕСТЫ
 print(unique_sorted([3, 1, 2, 1, 3]))
@@ -33,7 +33,7 @@ print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))"""
 
 
 #ТРЕТЬЯ ФУНКЦИЯ
-"""def flatten(mat: list[list | tuple]) -> list:
+def flatten(mat: list[list | tuple]) -> list:
     flattened_list = []
     for item in mat:
         if isinstance(item, (list, tuple)):
@@ -46,4 +46,4 @@ print(unique_sorted([1.0, 1, 2.5, 2.5, 0]))"""
 print(flatten([[1, 2], [3, 4]]))
 print(flatten(([1, 2], (3, 4, 5))))
 print(flatten([[1], [], [2, 3]]))
-print(flatten([[1, 2], "ab"]))"""
+print(flatten([[1, 2], "ab"]))
--- src/lab02/02_matrix.py
+++ src/lab02/02_matrix.py
@@ -1,15 +1,13 @@
 #ПЕРВАЯ ФУНКЦИЯ
-"""def transpose(mat: list[list[float | int]]) -> list[list]:
+def transpose(mat: list[list[float | int]]) -> list[list]:
     if not mat:
         return []
     
-    # Проверка на прямоугольность
     first_len = len(mat[0])
     for row in mat:
         if len(row) != first_len:
             raise ValueError("Рваная матрица")
     
-    # Создаем новую матрицу с размерами MxN -> NxM
     return [[mat[i][j] for i in range(len(mat))] for j in range(len(mat[0]))]
 
     
@@ -18,7 +16,7 @@ print(transpose([[1, 2, 3]]))
 print(transpose([[1], [2], [3]]))
 print(transpose([[1, 2], [3, 4]]))
 print(transpose([]))
-print(transpose([[1, 2], [3]]))"""
+print(transpose([[1, 2], [3]]))
 
 
 
@@ -44,7 +42,7 @@ print(row_sums([[1, 2], [3]]))"""
 
 
 #ТРЕТЬЯ ФУНКЦИЯ
-def col_sums(mat: list[list[float | int]]) -> list[float]:
+"""def col_sums(mat: list[list[float | int]]) -> list[float]:
     if not mat:
         return []
     
@@ -62,4 +60,4 @@ def col_sums(mat: list[list[float | int]]) -> list[float]:
 print(col_sums([[1, 2, 3], [4, 5, 6]]))
 print(col_sums([[-1, 1], [10, -10]]))
 print(col_sums([[0, 0], [0, 0]]))
-print(col_sums([[1, 2], [3]]))
+print(col_sums([[1, 2], [3]]))"""
