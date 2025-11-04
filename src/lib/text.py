95e7982cec5cbcc7e7c9caba3e77303a10d0e4ba
morenowtf <tvoibros@gmail.com>
2025-11-04 20:34:44 +0300

    fixes

--- src/lib/text.py
+++ src/lib/text.py
@@ -1,7 +1,7 @@
 import re
 
 def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
-    """Функция приводит текст к единому регистру и стилю"""
+    #Функция приводит текст к единому регистру и стилю
     s=text
     if casefold :
         s=s.casefold()
@@ -14,14 +14,14 @@ def normalize(text: str, *, casefold: bool = True, yo2e: bool = True) -> str:
     return s
 
 def tokenize(text: str) -> list[str]:
-    """Функция разбивает текст на «слова» по небуквенно-цифровым разделителям"""
+    #Функция разбивает текст на «слова» по небуквенно-цифровым разделителям
     pattern= r'\w+(?:-\w+)*'
     tokenstext = re.findall(pattern, text)
 
     return tokenstext
 
 def count_freq(tokens: list[str]) -> dict[str, int]:
-    """Функция возвращает словарь слово - количество"""
+    #Функция возвращает словарь слово - количество
     counts={}
     for word in tokens:
         counts[word]=counts.get(word,0)+1
@@ -31,7 +31,7 @@ def sort_key(item):
     return [-item[1], item[0]]
 
 def top_n(freq: dict[str, int], n: int = 5) -> list[tuple[str, int]]:
-    """Функция возвращает топ-N по убыванию частоты"""
+    #Функция возвращает топ-N по убыванию частоты
     sorted_freq= sorted(freq.items(),key=sort_key)
     top_n=[]
 
